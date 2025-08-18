f23bdd2f05198ca99cad7a04f63cd4af89eeb230
FireController1847 <usfirepilot123@gmail.com>
2025-08-17 23:02:35 -0600

    Migrate profiling project changes from CatalystUI

--- Crystal/Windowing/Profiling/Crystal.Windowing.Glfw3.Profiling/Program.cs
+++ Crystal/Windowing/Profiling/Crystal.Windowing.Glfw3.Profiling/Program.cs
@@ -12,19 +12,23 @@
 namespace Catalyst.Profiling {
     
     /// <summary>
-    /// A basic program used for general profiling of the Crystal Glfw3 windowing library.
+    /// A basic program used to general debugging and profiling of CatalystUI
+    /// and its associated libraries or tools.
     /// </summary>
     /// <remarks>
     /// <para>
-    /// This executable project references all Glfw3-related libraries. It references
-    /// a class called <c>Profile</c>, and fires a method with the signature
-    /// <c>static void Run()</c>.
+    /// The executable project references all associated libraries via their
+    /// project references to allow for debugging of the internals. It also
+    /// references a class called <c>Profile</c>, and fires a method with the
+    /// signature of <c>static void Entry()</c>.
     /// </para>
     /// <para>
-    /// <b>To use this project</b>, copy the <c>Profile.template.cs</c>
+    /// <b>To use the profiling project</b>, copy the <c>Profile.template.cs</c>
     /// file, and rename it to <c>Profile.cs</c>. Also rename the class
     /// from <c>ProfileTemplate</c> to <c>Profile</c>. Do not delete
     /// the template class, as that is committed to the repository.
+    /// You may also rename the <c>catdebug.template.ini</c> file to
+    /// <c>catdebug.ini</c> to enable debugging features.
     /// </para>
     /// <para>
     /// After creating the <c>Profile.cs</c> file, sometimes you
@@ -35,10 +39,16 @@ namespace Catalyst.Profiling {
     /// The profiling project is designed to be a simple way to profile
     /// sections of Catalyst's code, test performance, and/or generically
     /// test features of the library without needing to commit a new
-    /// testing project to the repository. This project only commits
+    /// testing project to the repository. It only commits
+    /// the necessary files to the repository, such as
     /// the csproj file, the Program file, and the template file,
     /// with special provisions made in the `.gitignore` to ensure
     /// no other files are committed.
+    /// </para>
+    /// <para>
+    /// Please avoid modifications to the project file itself as
+    /// much as possible to avoid git conflicts. If modification
+    /// is unavoidable, ensure the changes do not get committed.
     /// </para>
     /// </remarks>
     public static class Program {
